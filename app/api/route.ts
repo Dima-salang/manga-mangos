743c703a4ed6a71e687bcbc99d0080ea3c95b9ca
Dima-salang <luisgabrielle1026@gmail.com>
2026-02-15 19:28:58 +0800

    add: abstracted navbar into a component instead of hardcoding inside page

--- app/api/route.ts
+++ app/api/route.ts
@@ -1,7 +1,1 @@
 import { NextResponse } from 'next/server';
-import { getAllManga } from '@/lib/services/manga.service';
-
-export async function GET() {
-  const manga = await getAllManga();
-  return NextResponse.json(manga);
-}
