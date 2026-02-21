af7105547bf2da18e61cf2e639690e2b7ce126f3
Dima-salang <luisgabrielle1026@gmail.com>
2026-02-21 23:09:52 +0800

    add: caching the library context, user library, and top manga

--- lib/services/manga.service.ts
+++ lib/services/manga.service.ts
@@ -1,15 +1,21 @@
-import { MangaTypeFilter, TopMangaFilter, Manga, JikanResponse, MangaRecommendation, DB_MANGA } from '@/types/manga';
-import { LibraryItem, LibraryStatus } from '@/types/library';
-import { mangaFetch } from '@/lib/external-api/external-api';
-import { supabaseAdmin } from '@/utils/supabase/server';
-import { redis } from '@/utils/upstash-redis/redis';
-
+import {
+  MangaTypeFilter,
+  TopMangaFilter,
+  Manga,
+  JikanResponse,
+  MangaRecommendation,
+  DB_MANGA,
+} from "@/types/manga";
+import { LibraryItem, LibraryStatus } from "@/types/library";
+import { mangaFetch } from "@/lib/external-api/external-api";
+import { supabaseAdmin } from "@/utils/supabase/server";
+import { redis } from "@/utils/upstash-redis/redis";
 
 export async function getTopManga(
   type: MangaTypeFilter = MangaTypeFilter.MANGA,
   filter: TopMangaFilter = TopMangaFilter.BY_POPULARITY,
   page: number = 1,
-  limit: number = 25
+  limit: number = 25,
 ): Promise<JikanResponse<Manga[]>> {
   const queryParams = new URLSearchParams({
     type,
@@ -18,7 +24,28 @@ export async function getTopManga(
     limit: limit.toString(),
   });
 
-  return await mangaFetch<JikanResponse<Manga[]>>(`top/manga?${queryParams.toString()}`);
+  // check the cache
+  const cachedTopManga = await redis.get<string>(
+    `topManga:${queryParams.toString()}`,
+  );
+  if (cachedTopManga) {
+    return JSON.parse(cachedTopManga) as JikanResponse<Manga[]>;
+  }
+
+  const response = await mangaFetch<JikanResponse<Manga[]>>(
+    `top/manga?${queryParams.toString()}`,
+  );
+
+  // set the cache
+  await redis.set(
+    `topManga:${queryParams.toString()}`,
+    JSON.stringify(response),
+    {
+      ex: 60 * 60 * 24,
+    },
+  );
+
+  return response;
 }
 
 // manga service used
@@ -27,15 +54,18 @@ export class MangaService {
     type: MangaTypeFilter = MangaTypeFilter.MANGA,
     filter: TopMangaFilter = TopMangaFilter.BY_POPULARITY,
     page: number = 1,
-    limit: number = 25
+    limit: number = 25,
   ): Promise<JikanResponse<Manga[]>> {
     return getTopManga(type, filter, page, limit);
   }
 
-
   // search manga
-  async getSearchResults(queryParams: URLSearchParams): Promise<JikanResponse<Manga[]>> {
-    return await mangaFetch<JikanResponse<Manga[]>>(`manga?${queryParams.toString()}`);
+  async getSearchResults(
+    queryParams: URLSearchParams,
+  ): Promise<JikanResponse<Manga[]>> {
+    return await mangaFetch<JikanResponse<Manga[]>>(
+      `manga?${queryParams.toString()}`,
+    );
   }
 
   // get a specific manga
@@ -44,35 +74,57 @@ export class MangaService {
   }
 
   // get manga recommendations
-  async getMangaRecommendations(id: number): Promise<JikanResponse<MangaRecommendation[]>> {
-    return await mangaFetch<JikanResponse<MangaRecommendation[]>>(`manga/${id}/recommendations`);
+  async getMangaRecommendations(
+    id: number,
+  ): Promise<JikanResponse<MangaRecommendation[]>> {
+    return await mangaFetch<JikanResponse<MangaRecommendation[]>>(
+      `manga/${id}/recommendations`,
+    );
   }
 
   // get library items with manga details
-  async getLibraryWithManga(userId: string): Promise<(LibraryItem & { manga: DB_MANGA })[]> {
+  async getLibraryWithManga(
+    userId: string,
+  ): Promise<(LibraryItem & { manga: DB_MANGA })[]> {
+    // check the cache
+    const cachedLibrary = await redis.get<string>(`libraryWithManga:${userId}`);
+    if (cachedLibrary) {
+      return JSON.parse(cachedLibrary) as (LibraryItem & { manga: DB_MANGA })[];
+    }
+
     console.log("Fetching library with manga for user:", userId);
     const { data, error } = await supabaseAdmin
-      .from('library_item')
-      .select(`
+      .from("library_item")
+      .select(
+        `
         *,
         manga:mal_id (*)
-      `)
-      .eq('user_id', userId);
+      `,
+      )
+      .eq("user_id", userId);
 
     if (error) {
       throw error;
     }
 
+    // set the cache
+    await redis.set(`libraryWithManga:${userId}`, JSON.stringify(data), {
+      ex: 60 * 60 * 24,
+    });
+
     return data as (LibraryItem & { manga: DB_MANGA })[];
   }
 
   // get a specific library item
-  async getLibraryItem(userId: string, malId: number): Promise<LibraryItem | null> {
+  async getLibraryItem(
+    userId: string,
+    malId: number,
+  ): Promise<LibraryItem | null> {
     const { data, error } = await supabaseAdmin
-      .from('library_item')
-      .select('*')
-      .eq('user_id', userId)
-      .eq('mal_id', malId)
+      .from("library_item")
+      .select("*")
+      .eq("user_id", userId)
+      .eq("mal_id", malId)
       .maybeSingle();
 
     if (error) {
@@ -83,17 +135,23 @@ export class MangaService {
   }
 
   // add manga to library
-  async addMangaToLibrary(userId: string, manga: Manga, status: LibraryStatus, favorite: boolean): Promise<void> {
+  async addMangaToLibrary(
+    userId: string,
+    manga: Manga,
+    status: LibraryStatus,
+    favorite: boolean,
+  ): Promise<void> {
     // attempt to upsert the manga row
-    const { error: mangaError } = await supabaseAdmin
-      .from('manga')
-      .upsert({
+    const { error: mangaError } = await supabaseAdmin.from("manga").upsert(
+      {
         mal_id: manga.mal_id,
         images: manga.images,
         titles: manga.titles,
-      }, {
-        onConflict: 'mal_id'
-      });
+      },
+      {
+        onConflict: "mal_id",
+      },
+    );
 
     if (mangaError) {
       throw mangaError;
@@ -101,36 +159,45 @@ export class MangaService {
 
     // add or update the manga in the library
     const { error: libraryError } = await supabaseAdmin
-      .from('library_item')
-      .upsert({
+      .from("library_item")
+      .upsert(
+        {
           user_id: userId,
           mal_id: manga.mal_id,
           status,
           favorite,
-      }, {
-        onConflict: 'user_id, mal_id'
-      });
+        },
+        {
+          onConflict: "user_id, mal_id",
+        },
+      );
 
     if (libraryError) {
       throw libraryError;
     }
 
     // clear the cache
-    await redis.del(`library:${userId}`);
+    await Promise.all([
+      redis.del(`libraryWithManga:${userId}`),
+      redis.del(`libraryContext:${userId}`),
+    ]);
   }
 
   async removeMangaFromLibrary(userId: string, malId: number): Promise<void> {
     const { error: libraryError } = await supabaseAdmin
-      .from('library_item')
+      .from("library_item")
       .delete()
-      .eq('user_id', userId)
-      .eq('mal_id', malId);
+      .eq("user_id", userId)
+      .eq("mal_id", malId);
 
     if (libraryError) {
       throw libraryError;
     }
 
     // clear the cache
-    await redis.del(`library:${userId}`);
+    await Promise.all([
+      redis.del(`libraryWithManga:${userId}`),
+      redis.del(`libraryContext:${userId}`),
+    ]);
   }
 }
