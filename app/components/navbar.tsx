c4e6373b4df8769657e9491057acde86f23d7ab0
Emmanuel <emapalad09@gmail.com>
2026-02-22 22:48:46 +0800

    Add 'Reviews' link to protected navbar links

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -21,6 +21,7 @@ export function Navbar() {
 
   const protectedLinks = [
     { name: 'Library', href: '/library' },
+    { name: 'Reviews', href: '/reviews' },
     { name: 'Assistant', href: '/assistant' },
     { name: 'Profile', href: '/profile' },
   ];
